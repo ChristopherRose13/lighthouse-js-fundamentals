0c30f9f045a6b99e51ac03c7b758ff7be5c5ea1c
Christopher <christopherwrose2@gmail.com>
2021-12-27 16:46:39 -0800

    packing list updated

---
+++ packing-lists.js
@@ -0,0 +1,8 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
